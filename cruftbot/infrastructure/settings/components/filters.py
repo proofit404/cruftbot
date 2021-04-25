f7aee9e8cdd6bf5bdf5d52a7f165b4d3ef347ccf
Artem Malyshev <proofit404@gmail.com>
2021-04-25 03:36:06 +0300

    Setup django-filter package (#18).

---
+++ cruftbot/infrastructure/settings/components/filters.py
@@ -0,0 +1,5 @@
+"""Settings for django filter application."""
+from cruftbot.infrastructure.settings.components.base import INSTALLED_APPS
+
+
+INSTALLED_APPS.append("django_filters")
--- cruftbot/infrastructure/settings/production.py
+++ cruftbot/infrastructure/settings/production.py
@@ -3,7 +3,9 @@ from cruftbot.infrastructure.settings.components.activity_stream import *
 from cruftbot.infrastructure.settings.components.axes import *
 from cruftbot.infrastructure.settings.components.base import *
 from cruftbot.infrastructure.settings.components.content_security_policy import *
+from cruftbot.infrastructure.settings.components.filters import *
 from cruftbot.infrastructure.settings.components.node_assets import *
+from cruftbot.infrastructure.settings.components.permissions_policy import *
 from cruftbot.infrastructure.settings.components.staticfiles import *
 from cruftbot.infrastructure.settings.components.templates import *
 
