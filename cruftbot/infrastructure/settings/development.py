bff98e5a9cb8652e80718992ce87bcf570081262
Artem Malyshev <proofit404@gmail.com>
2021-04-26 20:38:43 +0300

    Reuse components definition from the production file (#230).

--- cruftbot/infrastructure/settings/development.py
+++ cruftbot/infrastructure/settings/development.py
@@ -1,27 +1,12 @@
 """Framework settings for local development."""
-from cruftbot.infrastructure.settings.components.activity_stream import *
-from cruftbot.infrastructure.settings.components.axes import *
-from cruftbot.infrastructure.settings.components.base import *
 from cruftbot.infrastructure.settings.components.base import CORS_ALLOWED_ORIGINS
-from cruftbot.infrastructure.settings.components.content_security_policy import *
 from cruftbot.infrastructure.settings.components.debug_toolbar import *
 from cruftbot.infrastructure.settings.components.extensions import *
 from cruftbot.infrastructure.settings.components.extra_checks import *
-from cruftbot.infrastructure.settings.components.filters import *
-from cruftbot.infrastructure.settings.components.guardian import *
 from cruftbot.infrastructure.settings.components.migration_linter import *
-from cruftbot.infrastructure.settings.components.node_assets import *
-from cruftbot.infrastructure.settings.components.permissions_policy import *
 from cruftbot.infrastructure.settings.components.querycount import *
-from cruftbot.infrastructure.settings.components.staticfiles import *
-from cruftbot.infrastructure.settings.components.templates import *
 from cruftbot.infrastructure.settings.components.test_migrations import *
-
-
-# @todo #183 Reuse components definition from the production file.
-#  Development settings is simply an extension of used components
-#  in addition to the production list. It would be nice not to
-#  repeat components above this comment.
+from cruftbot.infrastructure.settings.production import *
 
 
 DEBUG = True
