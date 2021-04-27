cc504809fc3547733f55b01eee7cf44dafa7c589
Artem Malyshev <proofit404@gmail.com>
2021-04-27 17:28:15 +0300

    Setup django-reversion package (#23).

--- cruftbot/infrastructure/settings/production.py
+++ cruftbot/infrastructure/settings/production.py
@@ -8,6 +8,7 @@ from cruftbot.infrastructure.settings.components.guardian import *
 from cruftbot.infrastructure.settings.components.health_check import *
 from cruftbot.infrastructure.settings.components.node_assets import *
 from cruftbot.infrastructure.settings.components.permissions_policy import *
+from cruftbot.infrastructure.settings.components.reversion import *
 from cruftbot.infrastructure.settings.components.staticfiles import *
 from cruftbot.infrastructure.settings.components.templates import *
 
