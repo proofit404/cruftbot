0b4bc079b642c9a1031a34a8471006e51e85208b
Artem Malyshev <proofit404@gmail.com>
2021-05-04 11:09:58 +0300

    Do not lint third-party migrations (#312).

--- cruftbot/infrastructure/settings/development.py
+++ cruftbot/infrastructure/settings/development.py
@@ -1,5 +1,7 @@
 """Framework settings for local development."""
-from cruftbot.infrastructure.settings.components.base import CORS_ALLOWED_ORIGINS
+from cruftbot.infrastructure.settings.components.cross_origin_resource_sharing import (
+    CORS_ALLOWED_ORIGINS,
+)
 from cruftbot.infrastructure.settings.components.debug_toolbar import *
 from cruftbot.infrastructure.settings.components.extensions import *
 from cruftbot.infrastructure.settings.components.extra_checks import *
