de44287013eb6d390c475a41b7d36a70d1a7d935
Artem Malyshev <proofit404@gmail.com>
2021-05-09 00:21:32 +0300

    Set HSTS setting (#333).

--- cruftbot/infrastructure/settings/components/security.py
+++ cruftbot/infrastructure/settings/components/security.py
@@ -5,3 +5,7 @@ from cruftbot.infrastructure.settings.components.base import MIDDLEWARE
 MIDDLEWARE.insert(0, "django.middleware.security.SecurityMiddleware")
 
 SECURE_REFERRER_POLICY = "no-referrer"
+
+SECURE_HSTS_SECONDS = 31536000
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
