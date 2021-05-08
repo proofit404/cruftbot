102034bd1a27144d2c1f6a8f9dc50f8d097ffc29
Artem Malyshev <proofit404@gmail.com>
2021-05-09 00:49:50 +0300

    Set SESSION_COOKIE_SECURE setting (#336).

---
+++ cruftbot/infrastructure/settings/components/session.py
@@ -0,0 +1,10 @@
+"""Settings for session contrib application."""
+from cruftbot.infrastructure.settings.components.base import INSTALLED_APPS
+from cruftbot.infrastructure.settings.components.base import MIDDLEWARE
+
+
+INSTALLED_APPS.append("django.contrib.sessions")
+
+MIDDLEWARE.append("django.contrib.sessions.middleware.SessionMiddleware")
+
+SESSION_COOKIE_SECURE = True
